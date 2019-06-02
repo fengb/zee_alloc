8f101977f93b90ad212f50fe97a3e8d35f5bb27a
Benjamin Feng <benjamin.feng@glassdoor.com>
2019-06-02 14:04:30 -0500

    Better consistency with C api

--- src/wasm_exports.zig
+++ src/wasm_exports.zig
@@ -1,12 +1,13 @@
 const builtin = @import("builtin");
 const zee_alloc = @import("main.zig");
 
-export fn malloc(size: usize) ?[*]u8 {
+export fn malloc(size: usize) ?*c_void {
     var result = zee_alloc.wasm_allocator.alloc(u8, size) catch return null;
     return result.ptr;
 }
 
-export fn free(ptr: [*]u8) void {
-    // Use a synthetic slice. zee_alloc should free via metadata.
+export fn free(c_ptr: *c_void) void {
+    // Use a synthetic slice. zee_alloc will free via corresponding metadata.
+    const ptr = @ptrCast([*]u8, c_ptr);
     zee_alloc.wasm_allocator.free(ptr[0..1]);
 }
